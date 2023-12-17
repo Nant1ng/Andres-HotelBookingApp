ca3856c659905322da5935e3b84e6d129e4dea5c
Nant1ng <andressantana99@hotmail.se>
2023-12-17 14:20:51 +0100

    Added Libary, with Enum and Interface

---
+++ Library&Services/Enumeration/RoomType.cs
@@ -0,0 +1,8 @@
+﻿namespace Library_Services.Enumeration
+{
+    public enum RoomType
+    {
+        SingleRoom,
+        DoubleRoom
+    }
+}
---
+++ Library&Services/Interface/IEntityManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Library_Services.Interface
+{
+    public interface IEntityManager
+    {
+        void Create();
+        void GetOne();
+        void GetAll();
+        void Update();
+        void Delete();
+    }
+}
