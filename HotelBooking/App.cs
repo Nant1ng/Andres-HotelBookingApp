e5d900d06cec42a581285056e634e8d62e8d2eb2
Nant1ng <andressantana99@hotmail.se>
2024-01-03 18:02:15 +0100

    App

--- HotelBooking/App.cs
+++ HotelBooking/App.cs
@@ -1,10 +1,66 @@
-﻿namespace HotelBooking
+﻿using LibraryAndService.Menu;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelBooking
 {
     public class App
     {
         public static void Run()
         {
-            Console.WriteLine("Qwer");
+            //var builder = new ConfigurationBuilder()
+            //   .AddJsonFile($"appsettings.json", true, true);
+            //var config = builder.Build();
+
+            do
+            {
+                Console.WriteLine(@"
+        ██╗  ██╗ ██████╗ ████████╗███████╗██╗         ███████╗ █████╗ ███╗   ██╗████████╗ █████╗ ███╗   ██╗ █████╗ 
+        ██║  ██║██╔═══██╗╚══██╔══╝██╔════╝██║         ██╔════╝██╔══██╗████╗  ██║╚══██╔══╝██╔══██╗████╗  ██║██╔══██╗
+        ███████║██║   ██║   ██║   █████╗  ██║         ███████╗███████║██╔██╗ ██║   ██║   ███████║██╔██╗ ██║███████║
+        ██╔══██║██║   ██║   ██║   ██╔══╝  ██║         ╚════██║██╔══██║██║╚██╗██║   ██║   ██╔══██║██║╚██╗██║██╔══██║
+        ██║  ██║╚██████╔╝   ██║   ███████╗███████╗    ███████║██║  ██║██║ ╚████║   ██║   ██║  ██║██║ ╚████║██║  ██║
+        ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+                                                                                                                   
+                                                    [1] Booking
+
+                                                    [2] Guest
+
+                                                    [3] Room
+
+                                                    [0] Exit  
+                ");
+
+                char key = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+                Booking booking = new Booking();
+                Guest guest = new Guest();
+                Room room = new Room();
+
+                switch (key)
+                {
+                    case '1':
+                        booking.Menu();
+                        break;
+
+                    case '2':
+                        guest.Menu();
+                        break;
+
+                    case '3':
+                        room.Menu();
+                        break;
+
+                    case '0':
+                        Console.WriteLine("Exiting...");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection. Please choose a valid option.");
+                        break;
+                }
+            } while (true);
         }
     }
 }
--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace HotelBooking
+﻿using LibraryAndService;
+
+namespace HotelBooking
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Hotel Santana";
+            Launch.Loading();
             App.Run();
         }
     }
