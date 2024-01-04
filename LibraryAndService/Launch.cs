2102ebda39ab9c763d32df986c8b4dca5834d3b1
Nant1ng <andressantana99@hotmail.se>
2024-01-04 23:58:22 +0100

    Launch

---
+++ LibraryAndService/Launch.cs
@@ -0,0 +1,30 @@
+﻿namespace LibraryAndService
+{
+    public class Launch
+    {
+        public static void Loading()
+        {
+            for (int seconds = 0; seconds <= 10; seconds++)
+            {
+                Console.WriteLine($"Loading{LoadingDots(seconds)}");
+                LoadingBar(seconds);
+                Thread.Sleep(500);
+                Console.Clear();
+            }
+        }
+
+        static void LoadingBar(int seconds)
+        {
+            int progressBarLength = 2 + seconds * 2;
+            string progressBar = new string('█', progressBarLength);
+            Console.Write(progressBar);
+        }
+
+        static string LoadingDots(int seconds)
+        {
+            int numDots = (seconds % 3) + 1;
+            string dots = new string('.', numDots);
+            return dots;
+        }
+    }
+}
