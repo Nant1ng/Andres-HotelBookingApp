cf03ccff6ae34210118475598d8dbb4cc55f39e9
Nant1ng <andressantana99@hotmail.se>
2024-01-05 20:55:35 +0100

    Change age to store as byte, and added booked

--- LibraryAndService/Models/Guest.cs
+++ LibraryAndService/Models/Guest.cs
@@ -14,18 +14,19 @@ namespace LibraryAndService.Models
         [MaxLength(250)]
         public string Email { get; set; }
         [Range(0, 100)]
-        public int? Age { get; set; }
+        public byte? Age { get; set; }
         public List<Booking?> Booking { get; set; }
-        public bool Booked { get; set; } = false; 
+        public bool Booked { get; set; } = false;
         public bool IsActive { get; set; }
 
-        public Guest(string firstName, string lastName, string phoneNumber, string email, int? age, bool isActive)
+        public Guest(string firstName, string lastName, string phoneNumber, string email, byte? age, bool booked, bool isActive)
         {
             FirstName = firstName;
             LastName = lastName;
             PhoneNumber = phoneNumber;
             Email = email;
             Age = age;
+            Booked = booked;
             IsActive = isActive;
         }
     }
