97d09630bf2c8670345e7c97d87fbfb9f1144a40
Nant1ng <andressantana99@hotmail.se>
2024-01-07 19:53:29 +0100

    Change MaxLength

--- LibraryAndService/Models/Guest.cs
+++ LibraryAndService/Models/Guest.cs
@@ -5,11 +5,11 @@ namespace LibraryAndService.Models
     public class Guest
     {
         public int Id { get; set; }
-        [MaxLength(100)]
+        [MaxLength(50)]
         public string FirstName { get; set; }
-        [MaxLength(100)]
+        [MaxLength(50)]
         public string LastName { get; set; }
-        [MaxLength(100)]
+        [MaxLength(50)]
         public string PhoneNumber { get; set; }
         [MaxLength(250)]
         public string Email { get; set; }
