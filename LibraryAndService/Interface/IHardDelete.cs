d41aed69ae6fdb9be867773e0abeb0a6fcc09855
Nant1ng <andressantana99@hotmail.se>
2024-01-07 15:47:48 +0100

    Added a new interface

---
+++ LibraryAndService/Interface/IHardDelete.cs
@@ -0,0 +1,10 @@
+﻿using LibraryAndService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAndService.Interface
+{
+    public interface IHardDelete
+    {
+        public void HardDelete(DbContextOptionsBuilder<ApplicationDbContext> options);
+    }
+}
