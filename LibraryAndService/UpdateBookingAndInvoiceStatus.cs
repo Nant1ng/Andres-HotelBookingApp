6e619a60d69838e62a0e7369a7206ea7816a9a22
Nant1ng <andressantana99@hotmail.se>
2024-01-08 20:31:39 +0100

    Done

---
+++ LibraryAndService/UpdateBookingAndInvoiceStatus.cs
@@ -0,0 +1,33 @@
+﻿using LibraryAndService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAndService
+{
+    /// <summary>
+    /// The CheckInvoiceDeadline method deactivates bookings and their invoices when the current date surpasses unpaid invoice deadlines. 
+    /// It identifies such bookings, sets them and their invoices as inactive, and saves these updates to the database, ensuring data integrity based on payment deadlines.
+    /// </summary>
+    public class UpdateBookingAndInvoiceStatus
+    {
+        public static void CheckInvoiceDeadline(DbContextOptionsBuilder<ApplicationDbContext> options)
+        {
+            using (ApplicationDbContext dbContext = new ApplicationDbContext(options.Options))
+            {
+                DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+
+                var bookingsToUpdate = dbContext.Booking
+                                                .Include(b => b.Invoice)
+                                                .Where(b => b.IsActive && b.Invoice != null && !b.Invoice.IsPayed && currentDate > b.Invoice.Deadline)
+                                                .ToList();
+
+                foreach (var booking in bookingsToUpdate)
+                {
+                    booking.IsActive = false;
+                    booking.Invoice.IsActive = false;
+                }
+
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
